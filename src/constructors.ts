c87b499e658edc85b58654a3d9ae8b82b65e34fa
Jason Lee <jlee388@ucr.edu>
2021-04-26 15:19:58 -0700

    pls

--- src/constructors.ts
+++ src/constructors.ts
@@ -4,27 +4,28 @@ import * as path from 'path';
 
 //Folder constructor function
 function makeFolder(folderPath: string, folderName: string) {
-    fs.mkdir(path.resolve(folderPath, folderName), err => {
-      // if err, display error message
-      if (err) {
-        console.log("err: ", err);
-        return vscode.window.showErrorMessage(
-          `Failed to create ${folderName}!`
-        );
-      }
-    });
+  fs.mkdir(path.resolve(folderPath, folderName), err => {
+    // if err, display error message
+    if (err) {
+      console.log("err: ", err);
+      return vscode.window.showErrorMessage(
+        `Failed to create ${folderName}!`
+      );
+    }
+  });
 }
 
 //File constructor function
 function makeFile(filePath: string, fileName: string, fileString: string) {
-    fs.writeFile(path.resolve(filePath, fileName), fileString, err => {
-        // if err, display error message
-        if (err) {
-            console.log("err: ", err);
-        return vscode.window.showErrorMessage(
-            `Failed to create ${fileName}!`
-        );}
-    });
+  fs.writeFile(path.resolve(filePath, fileName), fileString, err => {
+    // if err, display error message
+    if (err) {
+      console.log("err: ", err);
+      return vscode.window.showErrorMessage(
+        `Failed to create ${fileName}!`
+      );
+    }
+  });
 }
 
 //FileSync constructor function
--- src/extension.ts
+++ src/extension.ts
@@ -80,4 +80,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
