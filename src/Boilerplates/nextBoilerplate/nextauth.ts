e51244827c95dda1c4f6d776916f238c99d285b8
Jason Lee <jlee388@ucr.edu>
2021-05-08 16:56:54 -0700

    updated dynamic route nextauth to have OAuth capabilities with Twitter, Facebook, and Google
    Co-authored-by: jorgecarlosfernandez <jcferna1@gmail.com>
    Co-authored-by: peterhyesung <hyesunglee91@gmail.com>
    Co-authored-by: andrewkimq <andrewkimq@gmail.com>
    Co-authored-by: kaijosefevans <kaijosefevans@gmail.com>

--- src/Boilerplates/nextBoilerplate/nextauth.ts
+++ src/Boilerplates/nextBoilerplate/nextauth.ts
@@ -6,17 +6,29 @@ const options = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // Providers.Email({
-    //   server: {
-    //     host: "",
-    //     port: "",
-    //     auth: {
-    //       user: "",
-    //       pass: "",
-    //     } 
-    //   },
-    //   from: "",
-    // }),
+    Providers.Twitter({
+      clientId: process.env.TWITTER_ID,
+      clientSecret: process.env.TWITTER_SECRET,
+    }),
+    Providers.Facebook({
+      clientId: process.env.FACEBOOK_ID,
+      clientSecret: process.env.FACEBOOK_SECRET,
+    }),
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    Providers.Email({
+      server: {
+        host: "",
+        port: "",
+        auth: {
+          user: "",
+          pass: "",
+        } 
+      },
+      from: process.env.EMAIL_FROM,
+    }),
   ]
 };
 export default (req, res) => NextAuth(req, res, options);
