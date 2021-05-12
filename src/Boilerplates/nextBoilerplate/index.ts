2a3d5eb875fc3e0394381c0ba46eb909aa8ce339
Kai <kaijosefevans@gmail.com>
2021-05-12 15:26:51 -0400

    edited psuedocode for /src/Boilerplates/nextBoilerplate/index.ts to have psuedocode to be broken up
    Co-authored-by: jorgecarlosfernandez <jcferna1@gmail.com>
    Co-authored-by: peterhyesung <hyesunglee91@gmail.com>
    Co-authored-by: andrewkimq <andrewkimq@gmail.com>
    Co-authored-by: jason-d-lee <jlee388@ucr.edu>

--- src/Boilerplates/nextBoilerplate/index.ts
+++ src/Boilerplates/nextBoilerplate/index.ts
@@ -9,13 +9,15 @@ function HomePage() {
 
   return (
     <main>
-      // If the session returns false, the user is not logged in, which will the display the following html at the '/' endpoint
+      // If the session returns false, the user is not logged in
+      // This will the display the following html at the '/' endpoint
       {!session && (
       <div>
         /* Using this button, will link you to the NextAuth signIn functionality, please update your .env to include the information needed to use this, to learn more on the setup process, please view this link: https://next-auth.js.org/configuration/providers#oauth-providers
         */
         <button onClick={signIn}>Sign In</button>
-        // On click of the signup button, the button will rout you to /signup endpoint where user creation through graphQL will take place
+        // On click of the signup button, the button will route you to the /signup endpoint
+        // This is where user creation through graphQL will take place
         <Link href='/signup'>
           <button>Sign Up</button>
         </Link>
@@ -24,12 +26,14 @@ function HomePage() {
         <p>To edit this page, add to index.js in your pages directory.</p>
      </div>
     )}
-    // If the session returns true, the user is logged in, which will the display the following html at the '/' endpoint
+    // If the session returns true, the user is logged in
+    // This will the display the following html at the '/' endpoint
     {session && (
       <div>
       // The user will be informed below that they are signed in
         Signed in
-        // Since the user is now signed in, they have access to the checkout page which is binded to a button with the inner text of Payment
+        // Since the user is now signed in
+        // The user has access to the checkout page which is binded to a button with the inner text of Payment
         <Link href="/checkout">
           <button>Payment</button>
         </Link>
