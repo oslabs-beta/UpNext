95b572f683c905e5ea7704cd448e5c01c6a7c48a
andrewkimq <andrewkimq@gmail.com>
2021-05-12 11:48:14 -0700

    Fixed typos in pseudoCode

--- out/Boilerplates/stripeBoilerplate/checkoutBoilerplate.js
+++ out/Boilerplates/stripeBoilerplate/checkoutBoilerplate.js
@@ -3,14 +3,15 @@ const checkoutString = `
 //loadStripe is a function that returns a promise with a newly created Stripe Object.
 import { loadStripe } from '@stripe/stripe-js';
 
-//We initialize an instance of the Stripe object passing in the Stripe Public API key to identify the user's website to Stripe and assign it the variable Stripe Promise.
+//We initialize an instance of the Stripe object passing in the Stripe Public API key from the env file
+//to identify the user's website to Stripe and assign it the variable stripePromise.
 const stripePromise = loadStripe(
   \`\${process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY}\`
 );
 
 //This is a react component which renders some text and a button and also has a method to handle the funtionality of the button.
 export default function Checkout() {
-  //The function handleClick will fetch the session from an API endpoint, by sending a Post request and receving the session from the response.   
+  //The function handleClick will fetch the session from an API endpoint, by sending a Post request and receving a session from the response.   
   const handleClick = async (event) => {
     const { sessionId } = await fetch('/api/checkout/session', {
       method: 'POST',
@@ -23,7 +24,8 @@ export default function Checkout() {
     //Resolves the promise for the instance of the stripe Object providing the entry point for stripe development tools
     const stripe = await stripePromise;
 
-    //Resolves the promise for redirecting to Checkout, a Stripe-hosted page to securely collect payment information, using the provided session ID.
+    //Resolves the promise for the method redirectingtoCheckout for the stripe object, a Stripe-hosted page to 
+    //securely collect payment information, using the provided session ID.
     //We also destructure the error key from the redirecToCheckout object for future debugging purposes.
     const { error } = await stripe.redirectToCheckout({
       sessionId,
